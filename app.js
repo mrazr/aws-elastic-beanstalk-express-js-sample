31d5be98db85526972021ecac85fdb543096dd1f
Radoslav Mráz <radoslav.mraz95@gmail.com>
2024-03-08 20:01:48 +0100

    edit greeting

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Good news, everyone! Howdy'));
+app.get('/', (req, res) => res.send('Good news, everyone! Ciao'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
